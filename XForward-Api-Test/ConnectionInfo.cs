d91cd0220befce42c6704f5f5e115a52b2301e33
Navid Faridi <navidfaridi@yahoo.com>
2022-02-22 11:58:15 +0300

    add
    add simple project

---
+++ XForward-Api-Test/ConnectionInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace WA_XForward_Api_Test
+{
+    public class StringKeyValue
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+    public class ConnectionInfo
+    {
+        public string LocalIpAddress { get; set; }
+        public int LocalPort { get; set; }
+        public string RemoteIpAddress { get; set; }
+        public int RemotePort { get; set; }
+        public string RequestHost { get; set; }
+        public string RequestScheme { get; set; }
+        public List<StringKeyValue> HeaderData { get; set; }
+    }
+}
---
+++ XForward-Api-Test/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WA_XForward_Api_Test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetClientIp()
+        {
+            var connectionInfo = new ConnectionInfo
+            {
+                LocalIpAddress = HttpContext.Connection.LocalIpAddress.ToString(),
+                LocalPort = HttpContext.Connection.LocalPort,
+                RemoteIpAddress = HttpContext.Connection.RemoteIpAddress.ToString(),
+                RemotePort = HttpContext.Connection.RemotePort,
+                RequestHost = HttpContext.Request.Host.ToString(),
+                RequestScheme = HttpContext.Request.Scheme
+            };
+            connectionInfo.HeaderData = new List<StringKeyValue>();
+            foreach (var k in HttpContext.Request.Headers.Keys)
+                connectionInfo.HeaderData.Add(new StringKeyValue() { Key = k, Value = HttpContext.Request.Headers[k] }); 
+            return Ok(connectionInfo);
+        }
+    }
+}
